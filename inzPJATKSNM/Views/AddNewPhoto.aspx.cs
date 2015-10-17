2bf01768ba1e068a182a3804e38282ebde6fcfd9
kmiakisz <kubelek1@vp.pl>
2015-10-18 00:42:02 +0200

    dziwniejsze rzeczy

--- inzPJATKSNM/Views/AddNewPhoto.aspx.cs
+++ inzPJATKSNM/Views/AddNewPhoto.aspx.cs
@@ -35,6 +35,7 @@ namespace inzPJATKSNM.Views
             technikaId = int.Parse(TechnikaDropDownList.SelectedValue);
             kategoriaId = int.Parse(KategoriaDropDownList.SelectedValue);
             autorId = int.Parse(AutorDropDownList.SelectedValue);
+            filePath = filePath.Replace('\\', '/');
             inzPJATKSNM.Controllers.AddPhotoController.storePhotoToDb(filePath, technikaId, kategoriaId, autorId);
         }
 
