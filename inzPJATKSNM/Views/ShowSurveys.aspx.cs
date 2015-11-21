545e27991600b8273ad8585660edad747786f029
kmiakisz <kubelek1@vp.pl>
2015-11-21 23:48:36 +0100

    deleting surveys

--- inzPJATKSNM/Views/ShowSurveys.aspx.cs
+++ inzPJATKSNM/Views/ShowSurveys.aspx.cs
@@ -1,7 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
+using System.Web.Services;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
@@ -9,14 +13,19 @@ namespace inzPJATKSNM.Views
 {
     public partial class ShowSurveys : System.Web.UI.Page
     {
-        Dictionary<int, String> nazwyAnkiet;
-        Dictionary<int, String> opisyAnkiet;
-        Dictionary<int, String> urlAnkiet;
+        Dictionary<int, String> nazwyAnkiet = new Dictionary<int,string>();
+        Dictionary<int, String> opisyAnkiet = new Dictionary<int, string>();
+        Dictionary<int, String> urlAnkiet = new Dictionary<int, string>();
         public int liczbaAnkiet = 0;
+        public int CsVariable = 0;
         protected void Page_Load(object sender, EventArgs e)
         {
-            LoadSurveysFromDb();
-            liczbaAnkiet = policzAnkiety();
+           
+                LoadSurveysFromDb();
+                liczbaAnkiet = policzAnkiety();
+              
+            
+                       
         }
         public void LoadSurveysFromDb()
         {
@@ -42,6 +51,21 @@ namespace inzPJATKSNM.Views
         public Dictionary<int, String> getNazwyDict()
         {
             return nazwyAnkiet;
+        }
+        public String getButtonId(String id)
+        {
+            String buttonID = "";
+            Button button = (Button)FindControl(id);
+            return buttonID;
+        }
+        [WebMethod]
+        public static void usunAnkiete(int id)
+        {
+            inzPJATKSNM.Controllers.ShowSurveysController.removeSurvey(id);
+            
+        }
+        public void refreshSite(){
+            Response.Redirect("ShowSurveys.aspx");
         }
     }
 }
