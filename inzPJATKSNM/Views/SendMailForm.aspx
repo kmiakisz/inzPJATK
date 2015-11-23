8ffe18b96868a9d50e684c20f108f97e359f109e
mateuszonasz <s10891@pjwstk.edu.pl>
2015-11-23 22:53:24 +0100

    smtp client configured

---
+++ inzPJATKSNM/Views/SendMailForm.aspx
@@ -0,0 +1,4 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="SendMailForm.aspx.cs" Inherits="inzPJATKSNM.Views.SendMailForm" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <asp:Button ID="Button1" runat="server" Text="Button" OnClick="Button1_Click" />
+</asp:Content>
---
+++ inzPJATKSNM/Views/SendMailForm.aspx.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace inzPJATKSNM.Views
+{
+    public partial class SendMailForm : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            MailMessage message = new MailMessage();
+            message.From = new MailAddress("ankietySNM@gmail.com");
+
+
+            message.To.Add(new MailAddress("s10509@pjwstk.edu.pl"));
+
+            message.Body = "testowy mal";
+            message.CC.Add(new MailAddress("mateuszonasz@gmail.com"));
+
+            SmtpClient client = new SmtpClient();
+            client.Send(message);
+        }
+    }
+}
