566969ebcbf6a4889b595de4fe7dbe477c4eb7f6
kmiakisz <kubelek1@vp.pl>
2016-08-27 17:24:26 +0200

    Fixed problem with val and err

--- inzPJATKSNM/Views/AddNewPhoto.aspx.cs
+++ inzPJATKSNM/Views/AddNewPhoto.aspx.cs
@@ -17,7 +17,10 @@ namespace inzPJATKSNM.Views
         
         protected void Page_Load(object sender, EventArgs e)
         {
-            
+            if (Request.QueryString["err"] != null)
+            {
+                ScriptManager.RegisterStartupScript(this, this.GetType(), "pop", "failOpenModal();", true);
+            }
          
         }
 
@@ -35,25 +38,26 @@ namespace inzPJATKSNM.Views
                 catch (Exception ex)
                 {
                     errorMessage = ex.Message;
-                    Response.Redirect("ShowSurveys.aspx"+"?val="+errorMessage);
+                    Response.Redirect("ShowSurveys.aspx"+"?err="+errorMessage);
                 }
-                    
-                
+
+                technikaId = int.Parse(TechnikaDropDownList.SelectedValue);
+                kategoriaId = int.Parse(KategoriaDropDownList.SelectedValue);
+                autorId = int.Parse(AutorDropDownList.SelectedValue);
+                try
+                {
+                    inzPJATKSNM.Controllers.AddPhotoController.storePhotoToDb(filePath, technikaId, kategoriaId, autorId);
+                }
+                catch (Exception ex)
+                {
+                    errorMessage = ex.Message;
+                    Response.Redirect("ShowSurveys.aspx" + "?err=" + errorMessage);
+                }
             }else{
                 StatusLabel.Text="Nie wybrano żadnego pliku!!! .....";
+                Response.Redirect("AddNewPhoto.aspx" + "?err=" + "Nie udało się dodać zdjęcia! Spróbuj jeszcze raz!");
             }
-            technikaId = int.Parse(TechnikaDropDownList.SelectedValue);
-            kategoriaId = int.Parse(KategoriaDropDownList.SelectedValue);
-            autorId = int.Parse(AutorDropDownList.SelectedValue);
-            try
-            {
-                inzPJATKSNM.Controllers.AddPhotoController.storePhotoToDb(filePath, technikaId, kategoriaId, autorId);
-            }
-            catch (Exception ex)
-            {
-                errorMessage = ex.Message;
-                Response.Redirect("ShowSurveys.aspx" + "?val=" + errorMessage);
-            }
+            
         
         }
        
